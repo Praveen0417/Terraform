ce0e3872f3a6346cc8c7eb225a1e6673f3ccea5d
Praveen0417 <gaddam.praveen0417@gmail.com>
2024-07-04 17:56:25 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0a0e5d9c7acc336f1"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
